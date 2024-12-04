ed02b1d5e765a0a9d567a10b0b215338e97be606
prathmesh ladkat <prathameshladkat336@gmail.com>
2024-12-04 16:37:32 +0530

    bug fixes

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -7,11 +7,18 @@ const GptSearch = () => {
   return (
     <>
       <div className="fixed -z-10">
-        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
+        <img
+          className="h-screen object-cover w-screen"
+          src={BG_URL}
+          alt="logo"
+        />
       </div>
       <div className="">
         <GptSearchBar />
         <GptMovieSuggestions />
+        <h1 className="text-4xl font-bold text-center text-gray-800 my-8 p-6 bg-gradient-to-r from-yellow-200 to-yellow-400 rounded-lg shadow-lg transform -skew-y-1 hover:skew-y-0 transition-transform duration-300 ease-in-out">
+          This feature is under maintenance
+        </h1>
       </div>
     </>
   );
